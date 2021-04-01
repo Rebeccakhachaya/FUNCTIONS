3907a0ad156f63a98e819f76ab36ebd36771d162
rebeccakhachaya <rebeccakhachaya@gmail.com>
2021-04-01 23:43:48 +0300

    first assignment

---
+++ src/inheritance.kt
@@ -0,0 +1,12 @@
+fun main() {
+    var name: String = "Rebecca khachaya"
+    println(name)
+    var age = 21
+    println(age)
+    var phoneNumber = "0721217845"
+    println(phoneNumber)
+    var weight = 67
+    println(weight)
+    var citizen = "kenya"
+    print(citizen)
+}
